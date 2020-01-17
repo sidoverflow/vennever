d7bb934d63f3d28cb6e539a5bdc8914384d102b2
Sidharth Sudarsan <sidsarun@gmail.com>
2020-01-17 17:40:36 -0500

    prints blah blah with my name

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("branch of branch");
-		System.out.println("yo yo yo yo yo yo random yo");
 
-//		comment
-//		comment2
-		System.out.println("BIGG DIFFERENCE");
-		System.out.println("MUNEERS BRANCH");
 		System.out.println("Muneer");
 		System.out.println("Sid Sudarsan blah blah");
 		System.out.println("hello");
@@ -19,5 +13,5 @@ public class Main {
 		System.out.println("was just testing ");
 		System.out.println("Areeba Abidi");
 	}
-
+	
 }
