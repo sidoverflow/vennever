223d017848c0d634ab8553a647be0784a4fae21d
Sidharth Sudarsan <sidsarun@gmail.com>
2020-02-08 14:10:31 -0500

    implement main method with javafx entry point

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -1,16 +1,37 @@
 package venn;
 
-public class Main {
-
+import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+	 
+// Simple Hello World JavaFX program
+public class Main extends Application {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Muneer checking for conflict");
-		System.out.println("Sid Sudarsan blah blah");
-		System.out.println("hello CONFLICT");
-		
-		System.out.println("Lab task grading");
-
-	}
-
+        launch(args);
+    }
+	 
+// JavaFX entry point
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Demo.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}       
+    }
+	
+	
+	
+	
 }
+
